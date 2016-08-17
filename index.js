b62e76d8d60c9809f5231dbefa5ddf1d4fb96d54
Jeremy Robles <jeremy.robles56@gmail.com>
2016-08-17 16:15:41 -0700

    adding simple solution

--- index.js
+++ index.js
@@ -1,111 +1,81 @@
 var alexa = require('alexa-app'),
-	mongoose = require('mongoose'),
-
-	Schema = mongoose.Schema,
-	ObjectId = Schema.ObjectId,
-
-	getUrl = require('request');
-
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId,
+    joke = require('jokesearch');
 var app = new alexa.app();
+
+// getUrl.then = Promise;
+mongoose.Promise = Promise;
 
 //establish connection to the database
 
-// mongoose.connection('mongodb://alexa:asdf@ds161225.mlab.com:61225/jokes6000');
+mongoose.connect('mongodb://alexa:asdf@ds161225.mlab.com:61225/jokes6000');
 
 //database schema for jokes
 var JokeSchema = new Schema({
-	joke_id	  : Number,
-	title     : String,
-	selftext  : String
+    joke_id: Number,
+    title: String,
+    selftext: String
 });
 
 //provide a model
 var JokeModel = mongoose.model('jokes', JokeSchema);
 
-
-function getJoke(json){
-	getUrl(json, function (error, Response, data) {
-
-		if (!error && Response.statusCode == 200) {
-
-			var parsed = JSON.parse(data).data.children,
-				l = parsed.length,
-				n = Math.floor(Math.random() * (l - 1)) + 1,
-				title = parsed[n].data.title,
-				selftext = parsed[n].data.selftext;
-			console.log(title);
-			return title;
-
-		} else {
-			console.log(error);//log the error
-		}
-	});
-}
-
-
 /**
  * LaunchRequest.
  */
-app.launch(function(request,response) {
+app.launch(function (request, response) {
 
-	response.say(getJoke('https://www.reddit.com/r/cleanjokes/.json'));
+    /**
+     * Solution #1
+     */
+    joke.getJoke(function(joke) {
+        response.say(joke);
+        response.shouldEndSession(false);
+        response.send();
 
-	//check DB for the joke
+    });
 
-	//tell joke if it is not in the database
+    //check DB for the joke
 
-	//saves joke that was spoken to the DB
+    //tell joke if it is not in the database
+
+    //saves joke that was spoken to the DB
+    return false;
 });
-
-
-/**
- * IntentRequest.
- */
-app.intent('number',
-  {
-    'slots':{'number':'NUMBER'},
-    'utterances':[ 'say the number {1-100|number}' ]
-  },
-  function(request,response) {
-    var number = request.slot('number');
-	  response.session("test", 42);
-    response.say('You asked for the number '+number);
-    response.shouldEndSession(true);
-    response.send();
-  }
-);
 
 
 /**
  * IntentRequest w/ asynchronous response.
  */
-app.intent('checkStatus', 
-	{
-    	'utterances':[ 
-    		'status check', 'what is the status', 'tell me the status'
-    	]
-  	},
-	function(request,response) {
-		setTimeout(function() {		// simulate an async request
-			response.session("test", "omnius");
-	        // This is async and will run after a brief delay
-	        response.say('Status is operational, mam!');
-	    
-	        // Must call send to end the original request
-	        response.send();
-		
-		}, 250);
+app.intent('checkStatus',
+    {
+        'utterances': [
+            'status check', 'what is the status', 'tell me the status'
+        ]
+    },
+    function (request, response) {
+        setTimeout(function () {		// simulate an async request
+            response.session("test", "omnius");
+            // This is async and will run after a brief delay
+            response.say('Status is operational, mam!');
 
-	    // Return false immediately so alexa-app doesn't send the response
-	    return false;
-	}
+            // Must call send to end the original request
+            response.send();
+
+        }, 250);
+
+        // Return false immediately so alexa-app doesn't send the response
+        return false;
+    }
 );
 
 
 /**
  * Error handler for any thrown errors.
  */
-app.error = function(exception, request, response) {
+app.error = function (exception, request, response) {
     response.say('Sorry, something bad happened');
 };
 
--- test.js
+++ test.js
@@ -2,21 +2,44 @@
  * Created by jeremyrobles on 8/16/16.
  */
 
-var Request = require('request');
+var rp = require('request-promise');
+//
+// Request('https://www.reddit.com/r/cleanjokes/.json').then(function (data) {
+//     var parsed = JSON.parse(data).data.children,
+//         l = parsed.length,
+//         n = Math.floor(Math.random() * (l - 1)) + 1,
+//         title = parsed[n].data.title,
+//         selftext = parsed[n].data.selftext;
+//     console.log(title);
+//     console.log(selftext);
+//     // console.log("\n" + clor.blue.underline(title));
+//     // console.log(clor.yellow(selftext) + "\n\n\n");
+//     process.exit(1);
+// }).catch(function (err) {
+//     console.warn(err);
+//     process.exit(0);
+// });
 
-Request('https://www.reddit.com/r/cleanjokes/.json', function (error, Response, data) {
-    if (!error && Response.statusCode == 200) {
+var options = {
+    uri: 'https://www.reddit.com/r/cleanjokes/.json',
+    simple: false
+};
+
+rp(options)
+    .then(function (data) {
         var parsed = JSON.parse(data).data.children,
             l = parsed.length,
             n = Math.floor(Math.random() * (l - 1)) + 1,
             title = parsed[n].data.title,
             selftext = parsed[n].data.selftext;
-
-        if(typeof title === 'string'){
-            console.log(title);
-        }
-        // console.log(title, selftext);
-    } else {
-        console.log(error);//log the error
-    }
+        console.log(title);
+        console.log(selftext);
+        // response.say(title);
+        // response.shouldEndSession(false);
+        // response.send();
+    }).catch(function (err) {
+    console.log(err);
+    // response.say(err);
+    // response.shouldEndSession(false);
+    // response.send();
 });
